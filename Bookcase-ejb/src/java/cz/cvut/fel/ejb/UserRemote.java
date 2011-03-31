7cd779167b1b2111961fe3efb5bd255a7695d7d0
Tomáš Čerevka <tomas.cerevka@gmail.com>
2011-03-31 15:17:22 +0200

    Nakonfigurovano zakladni nastaveni. Nastaven ignore.

---
+++ Bookcase-ejb/src/java/cz/cvut/fel/ejb/UserRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.fel.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author tom
+ */
+@Remote
+public interface UserRemote {
+    
+}
